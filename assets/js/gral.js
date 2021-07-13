1e06e611773e63ac721344297d8113234ee2d8a7
Favio Lopez <faviolpz58@gmail.com>
2021-07-13 08:15:59 -0400

    creacion de menu

--- assets/js/gral.js
+++ assets/js/gral.js
@@ -3,5 +3,30 @@ $(document).ready(function () {
 })
 
 function base_url() {
-    return "/controlCamas/index.php/";
+    return "/controlCamas1.0/index.php/";
+}
+
+function cargarMenu() {
+    $.ajax({
+        type: 'POST',
+        url: base_url() + "Login/devolverMenu",
+        beforeSend: function (xhr) {
+            $("#menuPrincipal").empty();
+        }, success: function (data, textStatus, jqXHR) {
+            if (data != "null") {
+                var arr = JSON.parse(data);
+                var aux = "";
+                $.each(arr, function (index, contenido) {
+                    if (aux != contenido.grupo) {
+                        $("#menuPrincipal").append('<li class = "parent"><a href="#"><i class="fa fa-home"></i><span>' + contenido.grupo + '</span></a><ul class="sub-menu" id = "grupo-' + contenido.grupo + '"></ul></li>');
+                        $("#grupo-" + contenido.grupo).append('<li><a href="' + contenido.url + '"> ' + contenido.menu + '</a></li>');
+                        aux = contenido.grupo;
+                    } else {
+                        $("#grupo-" + contenido.grupo).append('<li><a href="' + contenido.url + '"> ' + contenido.menu + '</a></li>');
+                    }
+                })
+            } else {
+            }
+        }
+    })
 }
