b427aa5d51b9f245548bab541e8c67e42204355a
Favio Lopez <faviolpz58@gmail.com>
2021-07-30 15:10:43 -0400

    no message

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -1,6 +1,7 @@
 <?php
 
 defined('BASEPATH') or exit('No direct script access allowed');
+session_start();
 
 /**
  * Description of test
@@ -12,6 +13,9 @@ class test extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->helper('url');
+        if (!isset($_SESSION["usuario"])) {
+            redirect("login", "refresh");
+        }
     }
 
     public function index() {
